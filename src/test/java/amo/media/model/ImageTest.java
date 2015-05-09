0dacb5afd0c11da08fb91ef80690ab288ba9f134
Andreas Monger <andreas.monger@gmail.com>
2015-05-09 21:30:10 +0200

    test loops over folder and shows date

--- src/test/java/amo/media/model/ImageTest.java
+++ src/test/java/amo/media/model/ImageTest.java
@@ -15,17 +15,19 @@ import org.apache.tika.parser.AutoDetectParser;
 import org.apache.tika.parser.ParseContext;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
 public class ImageTest {
-    
+
     private static final String sep         = System.getProperty("file.separator");
     private final String        testdataDir = "src" + sep + "test" + sep + "resources" + sep + "testdata";
     private final File          testImage   = new File(this.testdataDir, "20140727_145200.jpeg");
-    
+
+    @Ignore
     @Test
     public void testFromFile() throws Exception {
         Image image = Image.fromFile(this.testImage);
@@ -33,19 +35,20 @@ public class ImageTest {
         // Desktop.getDesktop().open(this.testImage);
         // Assert.assertEquals(expected, image);
     }
-    
+
     private Tika        tika;
     private InputStream stream;
-    
+
     @Before
     public void setUp() throws FileNotFoundException {
         this.tika = new Tika();
         this.stream = new FileInputStream(this.testImage);
     }
-    
+
+    @Ignore
     @Test
     public void testImageMetadataCameraModel() throws IOException,
-            SAXException, TikaException {
+    SAXException, TikaException {
         Metadata metadata = new Metadata();
         ContentHandler handler = new DefaultHandler();
         // Parser parser = new JpegParser();
@@ -53,27 +56,55 @@ public class ImageTest {
         ParseContext context = new ParseContext();
         System.out.println(" ### SUPPORTED TYPES: " + parser.getSupportedTypes(context));
         String mimeType = this.tika.detect(this.stream); // needs to be
-                                                         // different input
-                                                         // stream ...
+        // different input
+        // stream ...
         System.out.println(" ### Recognized Type: " + mimeType);
         metadata.set(Metadata.CONTENT_TYPE, mimeType);
         parser.parse(new FileInputStream(this.testImage), handler, metadata, context);
         for (String name : metadata.names()) {
             System.out.println(" META[" + name + "]: " + metadata.get(name));
         }
-        
+
         // assertTrue("The expected Model is not correct", metadata.get("Model")
         // .equals("Canon EOS 350D DIGITAL"));
         Assert.assertEquals("2014-07-27T14:52:00", metadata.get("Creation-Date"));
     }
-    
+
+    @Test
+    public void testImageFolder() throws IOException, SAXException, TikaException {
+        ContentHandler handler = new DefaultHandler();
+        // Parser parser = new JpegParser();
+        AutoDetectParser parser = new AutoDetectParser();
+        File[] allTestFiles = new File(this.testdataDir).listFiles();
+        int fileCount = 0;
+        for (File file : allTestFiles) {
+            FileInputStream inStream = new FileInputStream(file);
+            String mimeType = this.tika.detect(inStream);
+            Metadata metadata = new Metadata();
+            metadata.set(Metadata.CONTENT_TYPE, mimeType);
+            parser.parse(new FileInputStream(file), handler, metadata, new ParseContext());
+            System.out.println("[" + fileCount++ + "]\t" + file.getName() + "\t " + mimeType + " \tdate: " + metadata.get("date")
+                    + "\t Creation-Date: " + metadata.get("Creation-Date"));
+            
+            // System.out.println(" ### Recognized Type: " + mimeType);
+            // for (String name : metadata.names()) {
+            // System.out.println(" META[" + name + "]: " + metadata.get(name));
+            // }
+            // System.in.read(new byte[1]);
+        }
+        // assertTrue("The expected Model is not correct", metadata.get("Model")
+        // .equals("Canon EOS 350D DIGITAL"));
+        // Assert.assertEquals("2014-07-27T14:52:00",
+        // metadata.get("Creation-Date"));
+    }
+
     @After
     public void close() throws IOException {
         if (this.stream != null) {
             this.stream.close();
         }
     }
-    
+
     // - See more at:
     // http://blog.jeroenreijn.com/2010/04/metadata-extraction-with-apache-tika.html#sthash.8KMgrrMi.dpuf
 }
